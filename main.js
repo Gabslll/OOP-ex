72c8f45c3df266d97656aa5cfbe53d1d919acb93
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-02-02 19:50:12 -0300

    Adjusting Classes

--- main.js
+++ main.js
@@ -38,9 +38,7 @@ function Speed(topSpeed, motor, topSpeed2) {
     this.topSpeed2 = topSpeed2;
 }
 
-const carroPessoal2 = new Carro("180sx", "Nissan", 2002, 1988);
 const carroPessoal2Speed = new Speed(340, ["SR20DE 150HP", "2.0L", "aspirado"]);
-const carroFabrica2 = new Carro("Silvia s13", "Nissan", 1998, 1989);
 const carroFabrica2Speed = new Speed(249, ["V8 biturbo", "3.9L", "720cv"]);
 const carroFuncionario2 = new Carro("BMW", "E30", 1994, 1981);
 const carroFuncionario2Speed = new Speed(248, ["4 cilindros", "2.3L", "240cv"]);
@@ -50,3 +48,34 @@ const vel = new Speed(carroPessoal2Speed, carroFabrica2Speed, carroFuncionario2S
 // console.log(Object.values(carroFuncionario2));
 
 // console.log(vel);
+
+class Veiculo {
+    constructor(modelo, marca, anoFabricacao, anoModelo) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.anoFabricacao = anoFabricacao;
+        this.anoModelo = anoModelo;
+    }
+}
+
+class CarroPessoal extends Veiculo {
+    constructor(modelo, marca, anoFabricacao, anoModelo, topSpeed, motor) {
+        super(modelo, marca, anoFabricacao, anoModelo);
+        this.topSpeed = topSpeed;
+        this.motor = motor;
+    }
+}
+
+class CarroFabrica extends Veiculo {
+    constructor(modelo, marca, anoFabricacao, anoModelo, topSpeed2, motor) {
+        super(modelo, marca, anoFabricacao, anoModelo);
+        this.topSpeed2 = topSpeed2;
+        this.motor = motor;
+    }
+}
+
+const carroPessoal2 = new CarroPessoal("180sx", "Nissan", 2002, 1988, 340, ["SR20DE 150HP", "2.0L", "aspirado"]);
+const carroFabrica2 = new CarroFabrica("Silvia s13", "Nissan", 1998, 1989, 249, ["V8 biturbo", "3.9L", "720cv"]);
+
+console.log(carroPessoal2);
+console.log(carroFabrica2);
--- maxSpeed.js
+++ maxSpeed.js
@@ -1,18 +1,63 @@
-const { carroFabrica2Speed, carroPessoal2Speed } = require('./main');
+const { topSpeed2, topSpeed } = require('./main');
 
 class Vel {
-    constructor() {
-        this.Speed1 = carroFabrica2Speed && carroPessoal2Speed;
+    constructor(carroPessoal, carroFabrica, carroFuncionario) {
+        this.carroPessoal = carroPessoal;
+        this.carroFabrica = carroFabrica;
+        this.carroFuncionario = carroFuncionario;
     }
 
     velMax() {
-        if (this.Speed1 < 250) {
-            return `A velocidade máxima é ${this.Speed1}`
+        const mensagem = [];
+
+        if (this.carroPessoal.topSpeed > 150) {
+            mensagem.push(`${this.carroPessoal.modelo} tem velocidade máxima de ${this.carroPessoal.topSpeed}`);
         } else {
-            return "Velocidade baixa";
+            mensagem.push(`${this.carroPessoal.modelo} tem velocidade baixa`);
         }
+
+        if (this.carroFabrica.topSpeed2 > 150) {
+            mensagem.push(`${this.carroFabrica.modelo} tem velocidade máxima de ${this.carroFabrica.topSpeed2}`);
+        } else {
+            mensagem.push(`${this.carroFabrica.modelo} tem velocidade baixa`);
+        }
+
+        if (this.carroFuncionario.topSpeed > 150) {
+            mensagem.push(`${this.carroFuncionario.modelo} tem velocidade máxima de ${this.carroFuncionario.topSpeed}`);
+        } else {
+            mensagem.push(`${this.carroFuncionario.modelo} tem velocidade baixa`);
+        }
+
+        return mensagem.join('\n');
     }
 }
 
-const velInstance = new Vel();
+const carroPessoal = {
+    modelo: '180sx',
+    marca: 'Nissan',
+    anoModelo: 2002,
+    anoFabricacao: 1988,
+    topSpeed: 340,
+    motor: ["SR20DE 150HP", "2.0L", "aspirado"]
+}
+
+const carroFabrica = {
+    modelo: 'Silvia 13',
+    marca: 'Nissan',
+    anoModelo: 1998,
+    anoFabricacao: 1989,
+    topSpeed2: 249,
+    motor: ["V8 biturbo", "3.9L", "720cv"]
+}
+
+const carroFuncionario = {
+    modelo: 'E30',
+    marca: 'BMW',
+    anoModelo: 1994,
+    anoFabricacao: 1981,
+    topSpeed: 248,
+    motor: ["4 cilindros", "2.3L", "240cv"]
+}
+
+const velInstance = new Vel(carroPessoal, carroFabrica, carroFuncionario);
 console.log(velInstance.velMax());
